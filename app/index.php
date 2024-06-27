d660b04748212acf60855228ead977bfc1e92dc9
doko972 david <doko972@gmail.com>
2024-06-27 13:52:06 +0200

    commit

--- app/index.php
+++ app/index.php
@@ -3,51 +3,42 @@ session_start();
 include "include/_config.php";
 include "include/_functions.php";
 
-// Génération ou vérification de token CSRF pour sécuriser les formulaires
 generateToken();
 
-// Ajouter une nouvelle tâche
-if (!empty($_POST['description']) && isset($_POST['button__add'])) {
-    refererHttp('index.php');
-    errorCsrf();
-
-    if (strlen($_POST['description']) <= 150) {
-        $insert = $dbConnect->prepare("INSERT INTO `task` (`priority`, `description`, `creation_date`, `done`) 
-            VALUES (:priority, :description, NOW(), 0);");
-        $insert->bindValue(':priority', htmlspecialchars($_POST['priority']));
-        $insert->bindValue(':description', htmlspecialchars($_POST['description']));
-        if ($insert->execute()) {
-            header('Location: index.php?msg=insert_ok');
-            exit;
-        }
-    }
-    header('Location: index.php?msg=insert_ko');
-    exit;
-}
-
-// Gérer les actions sur les tâches existantes
-if (!empty($_POST['task_id']) && !empty($_POST['token']) && $_POST['token'] === $_SESSION['token']) {
-    $taskId = intval($_POST['task_id']);
-    if ($_POST['action'] === 'delete') {
-        $delete = $dbConnect->prepare("DELETE FROM task WHERE id_task = :task_id");
-        $delete->bindParam(':task_id', $taskId, PDO::PARAM_INT);
-        if ($delete->execute()) {
-            header("Location: index.php?msg=delete_success");
-            exit;
-        }
-        header("Location: index.php?msg=delete_failure");
-        exit;
-    } elseif ($_POST['action'] === 'done') {
-        $update = $dbConnect->prepare("UPDATE task SET done = 1 WHERE id_task = :task_id");
-        $update->bindParam(':task_id', $taskId, PDO::PARAM_INT);
-        if ($update->execute()) {
-            header("Location: index.php?msg=done_success");
-            exit;
-        }
-        header("Location: index.php?msg=update_failure");
-        exit;
-    }
-}
+// if (!empty($_POST['task_id']) && !empty($_POST['token']) && $_POST['token'] === $_SESSION['token']) {
+//     $taskId = intval($_POST['task_id']);
+//     if ($_POST['action'] === 'delete') {
+//         $delete = $dbConnect->prepare("DELETE FROM task WHERE id_task = :task_id");
+//         $delete->bindParam(':task_id', $taskId, PDO::PARAM_INT);
+//         if ($delete->execute()) {
+//             header("Location: index.php?msg=delete_success");
+//             exit;
+//         }
+//         header("Location: index.php?msg=delete_failure");
+//         exit;
+//     }
+//     if ($_POST['action'] === 'done') {
+//         $update = $dbConnect->prepare("UPDATE task SET done = 1 WHERE id_task = :task_id");
+//         $update->bindParam(':task_id', $taskId, PDO::PARAM_INT);
+//         if ($update->execute()) {
+//             header("Location: index.php?msg=done_success");
+//             exit;
+//         }
+//         header("Location: index.php?msg=update_failure");
+//         exit;
+//     }
+//     if ($_POST['action'] === 'update' && !empty($_POST['new_description'])) {
+//         $update = $dbConnect->prepare("UPDATE task SET description = :description WHERE id_task = :task_id");
+//         $update->bindValue(':description', htmlspecialchars($_POST['new_description']), PDO::PARAM_STR);
+//         $update->bindParam(':task_id', $taskId, PDO::PARAM_INT);
+//         if ($update->execute()) {
+//             header("Location: index.php?msg=update_success");
+//             exit;
+//         }
+//         header("Location: index.php?msg=update_failure");
+//         exit;
+//     }
+// }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -58,32 +49,32 @@ if (!empty($_POST['task_id']) && !empty($_POST['token']) && $_POST['token'] === 
     <title>Liste de tâches</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+<header>
+    <nav class="navbar">
+        <div class="container__nav">
+            <input type="checkbox" name="" id="">
+            <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+            </div>
+            <ul class="menu-items">
+                <li><a href="index.php">Accueil</a></button></li>
+            </ul>
+        </div>
+    </nav>
+</header>
 
 <body>
     <section>
         <h1>Fiche de tâches</h1>
         <?php
-        $errors = [
-            'csrf' => 'Session invalide ou modifiée',
-            'referer' => 'Référent non autorisé',
-            'insert_ko' => 'Échec de l\'insertion de la tâche',
-            'delete_failure' => 'Échec de la suppression de la tâche',
-            'update_failure' => 'Échec de la mise à jour de la tâche'
-        ];
-        $messages = [
-            'insert_ok' => 'La tâche ajoutée.',
-            'delete_success' => 'La tâche supprimée.',
-            'done_success' => 'Tâche faites.'
-        ];
-
-        if (isset($_GET['msg'])) {
-            $msgType = isset($errors[$_GET['msg']]) ? $errors : $messages;
-            echo '<p>' . $msgType[$_GET['msg']] . '</p>';
-        }
+        echo getHtmlErrors($errors);
+        echo getHtmlMessages($messages);
         ?>
         <div class="container">
             <h2>Créer une tâche</h2>
-            <form method="POST" action="">
+            <form method="POST" action="actions.php">
                 <div class="task__list__create">
                     <input class="container__post--text" type="text" name="description"
                         placeholder="Ajouter chose(s) à faire" required>
@@ -93,7 +84,8 @@ if (!empty($_POST['task_id']) && !empty($_POST['token']) && $_POST['token'] === 
                         <option value="2">Moyen</option>
                         <option value="3">Bas</option>
                     </select>
-                    <button type="submit" name="button__add" class="button__add-task">Ajouter une tâche</button>
+                </div>
+                <button type="submit" name="buttonAdd" class="button__add-task">Ajouter une tâche</button>
             </form>
             <div class="task__list">
                 <div class="task-item">
@@ -107,26 +99,48 @@ if (!empty($_POST['task_id']) && !empty($_POST['token']) && $_POST['token'] === 
                             . '<li class="container__post--text">'
                             . htmlspecialchars($product['priority']) . ' - '
                             . htmlspecialchars($product['description']) . ' '
-                            . htmlspecialchars($product['creation_date']);
-
+                            . htmlspecialchars($product['creation_date'])
+                            . '</li>';
+                        //update text
+                        // echo '<li>'
+                        //     . '<form method="POST" action="actions.php">'
+                        //     . '<input type="hidden" name="task_id" value="'
+                        //     . htmlspecialchars($product['id_task'])
+                        //     . '">'
+                        //     . '<input type="hidden" name="token" value="' . $_SESSION['token'] . '">'
+                        //     . '<input type="hidden" name="action" value="update">'
+                        //     . '<input type="text" class="container__post--updatetext" name="new_description" value="'
+                        //     . htmlspecialchars($product['description'])
+                        //     . '" required>'
+                        //     . '<button type="submit" class="button__done">Enregistrer</button>'
+                        //     . '</form></li>';
                         //button delete
-                        echo '<form method="POST" action="" style="display: inline;">'
+                        echo '<li><form method="POST" action="actions.php" style="display: inline;">'
                             . '<input type="hidden" name="task_id" value="'
                             . htmlspecialchars($product['id_task']) . '">'
                             . '<input type="hidden" name="token" value="' . $_SESSION['token'] . '">'
                             . '<input type="hidden" name="action" value="delete">'
-                            . '<button type="submit" class="button__remove">x</button>'
-                            . '</form>'
-                            . '</ul>';
-                        //button done
-                        echo '<form method="POST" action="" style="display: inline;">'
-                        . '<input type="hidden" name="task_id" value="'
-                            . htmlspecialchars($product['id_task']) . '">'
-                        . '<input type="hidden" name="token" value="' . $_SESSION['token'] . '">'
-                        . '<input type="hidden" name="action" value="done">'
-                        . '<button type="submit" class="button__done">Tâche faite</button>'
-                        . '</form></li>';
+                            . '<button type="submit" class="button__done">Supprimer</button>'
+                            . '</form>';
 
+                        //button done
+                        echo '<form method="POST" action="actions.php" style="display: inline;">'
+                            . '<input type="hidden" name="task_id" value="'
+                            . htmlspecialchars($product['id_task']) . '">'
+                            . '<input type="hidden" name="token" value="' . $_SESSION['token'] . '">'
+                            . '<input type="hidden" name="action" value="done">'
+                            . '<button type="submit" class="button__done">Tâche faite</button>'
+                            . '</form>';
+
+                        //button update text
+                        echo '<form method="POST" action="actions.php" style="display: inline;">'
+                            . '<input type="hidden" name="task_id" value="'
+                            . htmlspecialchars($product['id_task']) . '">'
+                            . '<input type="hidden" name="token" value="' . $_SESSION['token'] . '">'
+                            . '<input type="hidden" name="action" value="updatetext">'
+                            . '<button type="submit" class="button__done">Modifier</button>'
+                            . '</form></li>';
+                        echo '</ul>';
                     } ?>
                 </div>
 
